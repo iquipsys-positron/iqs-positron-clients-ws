cd872509614a704b0e67185037c050810f3a6794
Sergey Seroukhov <seroukhov@gmail.com>
2020-01-29 13:28:34 -0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,25 @@
+# Workspace for iQuipsys Positron Client Applications
+
+## Overview
+
+iQuipsys Positron is an open-source IoT platform for Industrial Automation.
+It is an extensible and production ready solution that comes with communication,
+data storage and processing, configuration, administration and end-user tools.
+To learn more about iQuipsys Positron look at https://github.com/iquipsys-positron/iqs-positron
+
+This workspace allows quick developer's access to all iQuipsys client microapplications.
+
+## Usage
+
+1. Check out the workspace from GitHub
+
+`# git clone git clone git@github.com:iquipsys-positron/iqs-positron-clients-ps`
+
+2. Check out backend components
+`# cd iqs-positron-clients-ps`
+`# checkout.ps1`
+
+# Acknowledgements
+
+iQuipsys Positron is created and maintained by iQuipsys Inc.
+For any questions contact us at info@iquipsys.com
---
+++ checkout.ps1
@@ -0,0 +1,27 @@
+#!/usr/local/bin/pwsh
+
+git clone git@github.com:iquipsys-positron/iqs-devenv
+
+git clone git@github.com:iquipsys-positron/iqs-libs-clientshell-angular
+git clone git@github.com:iquipsys-positron/iqs-libs-clientshell2-angular.git
+
+git clone git@github.com:iquipsys-positron/iqs-clients-landing-angular.git
+git clone git@github.com:iquipsys-positron/iqs-clients-home-angular.git
+git clone git@github.com:iquipsys-positron/iqs-clients-monitoring-angular.git
+git clone git@github.com:iquipsys-positron/iqs-clients-retrospective-angular.git
+git clone git@github.com:iquipsys-positron/iqs-clients-routes-angular.git
+git clone git@github.com:iquipsys-positron/iqs-clients-statistics-angular.git
+git clone git@github.com:iquipsys-positron/iqs-clients-incidents-angular.git
+git clone git@github.com:iquipsys-positron/iqs-clients-schedule-angular.git
+git clone git@github.com:iquipsys-positron/iqs-clients-usersettings-angular.git
+git clone git@github.com:iquipsys-positron/iqs-clients-sensordata-angular.git
+git clone git@github.com:iquipsys-positron/iqs-clients-manualcorrections-angular.git
+
+git clone git@github.com:iquipsys-positron/iqs-clients-deviceconfig-angular.git
+git clone git@github.com:iquipsys-positron/iqs-clients-emergencyconfig-angular.git
+git clone git@github.com:iquipsys-positron/iqs-clients-eventconfig-angular.git
+git clone git@github.com:iquipsys-positron/iqs-clients-objectconfig-angular.git
+git clone git@github.com:iquipsys-positron/iqs-clients-orgconfig-angular.git
+git clone git@github.com:iquipsys-positron/iqs-clients-zoneconfig-angular.git
+
+git clone git@github.com:iquipsys-positron/iqs-clients-devicesimulator-angular.git
