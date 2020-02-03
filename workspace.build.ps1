bd494b37e011918a7edf1075f2d45dc38740236d
Aleksandr Koshevierov <iworb.ne@gmail.com>
2020-02-03 15:14:12 +0300

    Required packages and temporary workspace

---
+++ workspace.build.ps1
@@ -0,0 +1,36 @@
+function Publish-AWS-S3($dir, $bucket)
+{
+	'aws s3 sync ./' + $dir + '/dist s3://www.positron.iquipsys.net/' + $bucket | cmd
+}
+
+task Init {
+    Invoke-Task -Task Clone -Workspace
+    Invoke-Task -Task InstallDep -Workspace
+}
+
+task InstallDep {
+    Install-NpmDependencies -Path .
+}
+
+task Build {
+    # Proper sequence defined in $All variable it important!
+    Invoke-Task -Task Build -Component $All
+}
+
+task Clean {
+    Invoke-Task -Task Clean -All
+}
+
+task Publish-S3 {
+	$Name = (Get-Item -Path ".\").Name
+	$FullName = (Get-Item -Path ".\").FullName
+	If ($FullName -eq (Find-Workspace)) {
+		$global:Buckets.keys | ForEach-Object -Process {
+			Publish-AWS-S3 $_ $global:Buckets[$_]
+		}
+	}
+	Elseif ($global:Buckets.ContainsKey($Name)) {
+		Publish-AWS-S3 $Name $global:Buckets[$Name]
+	}
+}
+
