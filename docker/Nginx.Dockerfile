b50fbb1ffdea9aa25339a54dfc8fff5d245bac23
Aleksandr Koshevierov <iworb.ne@gmail.com>
2020-02-18 14:40:55 +0300

    Configurable client

--- docker/Nginx.Dockerfile
+++ docker/Nginx.Dockerfile
@@ -1,5 +1,14 @@
 FROM nginx:alpine
 
-COPY dist /usr/share/nginx/html
+# Install dependencies
+RUN apk add bash jq
+SHELL ["/bin/bash", "-c"]
+
+# set working directory
+WORKDIR /usr/share/nginx/html
+
+COPY dist .
+COPY docker/configure.sh ../configure.sh
 
 EXPOSE 80
+ENTRYPOINT ../configure.sh && /bin/bash
---
+++ docker/configure.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ $IQS_SERVER_URL ]]; then
+    js_configs=()
+    while IFS=  read -r -d $'\0'; do
+        js_configs+=("$REPLY")
+    done < <(find . -name config.js -print0)
+    for i in "${js_configs[@]}"; do
+        sed -ri "s/([\"']+serverUrl[\"']+:\s*[\"'])([^\"']*)([\"'])/\1$(echo $IQS_SERVER_URL | sed -e 's/[\/&]/\\&/g')\3/g" $i
+    done
+    json_configs=()
+    while IFS=  read -r -d $'\0'; do
+        json_configs+=("$REPLY")
+    done < <(find . -name config.json -print0)
+    for i in "${json_configs[@]}"; do
+        tmp=$(mktemp)
+        jq ".session.serverUrl = \"${IQS_SERVER_URL}\"" $i > "$tmp" && mv "$tmp" $i
+    done
+fi
